2d0ed0301f34c54f457334b6c229d510c223a3f1
MatiasFerullo <121054190+MatiasFerullo@users.noreply.github.com>
2025-03-26 14:00:58 -0300

    Update ejercicio_02.py

--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -29,7 +29,7 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
-    pass # Completar
+    return max(a,b,c,d)
 
 
 # NO MODIFICAR - INICIO
