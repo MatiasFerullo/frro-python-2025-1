e57fcae3500346a2a0e634e79b4120af38cc7846
Enzo Ariel <62959641+enzosanchezariel@users.noreply.github.com>
2025-08-16 21:52:28 +0000

    Agregar página dashboard

--- CarteraAcciones/flaskr/routes/index.py
+++ CarteraAcciones/flaskr/routes/index.py
@@ -5,4 +5,7 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
+    loggedIn = True
+    if (loggedIn):
+        return render_template('dashboard.html')
     return render_template('index.html')
