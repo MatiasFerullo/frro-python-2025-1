b118352587a84ee513ae8e914608d456ac05dcca
Enzo Ariel <enzo.sanchez.ariel@gmail.com>
2025-08-21 18:40:11 -0300

    Agregar páginas y diagramas placeholder a la vista

--- CarteraAcciones/flaskr/routes/index.py
+++ CarteraAcciones/flaskr/routes/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 
 index_bp = Blueprint('index', __name__)
@@ -9,3 +9,24 @@ def index():
     if (loggedIn):
         return render_template('dashboard.html')
     return render_template('index.html')
+
+@index_bp.route('/alerts')
+def alerts():
+    loggedIn = True
+    if (loggedIn):
+        return render_template('alerts.html')
+    return redirect(url_for('index.index'))
+
+@index_bp.route('/portfolio')
+def portfolio():
+    loggedIn = True
+    if (loggedIn):
+        return render_template('portfolio.html')
+    return redirect(url_for('index.index'))
+
+@index_bp.route('/chart')
+def chart():
+    loggedIn = True
+    if (loggedIn):
+        return render_template('chart.html')
+    return redirect(url_for('index.index'))
