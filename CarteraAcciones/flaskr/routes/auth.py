63dea08353f5ba289d4e8ad566e7e668d60206d7
Enzo Ariel Sánchez <enzo.sanchez.ariel@gmail.com>
2025-09-23 20:34:12 -0300

    Agregar persistencia de sesión

--- CarteraAcciones/flaskr/routes/auth.py
+++ CarteraAcciones/flaskr/routes/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect, render_template, request, jsonify, url_for
+from flaskr.services.message_service import render_message
+from flask import Blueprint, redirect, render_template, request, jsonify, url_for, session
 from flaskr.models import User
 from flaskr.services.fake_db import users
 
@@ -6,23 +7,47 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth_bp.route('/register')
 def registerPage():
+    if (session.get('user_id')):
+        return redirect(url_for('index.index'))
     return render_template('register.html')
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
     data = request.form
-    # if data['password_repeat'] == data['password']:
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    password_repeat = data['password_repeat']
+
+    if any(u.username == username for u in users):
+        return render_message(
+            "Error en el registro",
+            "El nombre de usuario ya existe. Por favor, elige otro nombre de usuario.",
+            "Volver",
+            url_for('auth.registerPage'))
+
+    if any(u.email == email for u in users):
+        return render_message(
+            "Error en el registro",
+            "El correo electrónico ya está registrado. Por favor, usa otro correo o inicia sesión.",
+            "Volver",
+            url_for('auth.registerPage'))
+
     new_user = User(
         id=0,
-        username=data['username'],
-        email=data['email'],
-        password=data['password']
+        username=username,
+        email=email,
+        password=password
     )
     users.append(new_user)
+
+    session['user_id'] = new_user.id
     return redirect(url_for('index.index'))
 
 @auth_bp.route('/login')
 def loginPage():
+    if (session.get('user_id')):
+        return redirect(url_for('index.index'))
     return render_template('login.html')
 
 @auth_bp.route('/login', methods=['POST'])
@@ -34,8 +59,18 @@ def login():
     user = next((u for u in users if u.username == username and u.password == password), None)
     
     if user:
+        session['user_id'] = user.id
         return redirect(url_for('index.index'))
-    return 'Credenciales inválidas'
+    return render_message(
+        "Credenciales inválidas",
+        "El usuario o contraseña son incorrectos.",
+        "Volver",
+        url_for('auth.loginPage'))
+
+@auth_bp.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index.index'))
 
 @auth_bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
